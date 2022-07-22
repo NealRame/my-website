b2bb03102b71fcc673435684e2b4382b9638d43f
Julien Graziano <julien@graziano.fr>
2022-07-22 23:46:48 +0200

    refactor: remove jello, wobble animation

--- src/style/animations.ts
+++ src/style/animations.ts
@@ -8,30 +8,6 @@ export const blink = keyframes`
     }
 `
 
-export const jello = keyframes`
-    0% {
-        transform: scale3d(1, 1, 1);
-    }
-    30% {
-        transform: scale3d(1.25, 0.75, 1);
-    }
-    40% {
-        transform: scale3d(0.75, 1.25, 1);
-    }
-    50% {
-        transform: scale3d(1.15, 0.85, 1);
-    }
-    65% {
-        transform: scale3d(0.95, 1.05, 1);
-    }
-    75% {
-        transform: scale3d(1.05, 0.95, 1);
-    }
-    100% {
-        transform: scale3d(1, 1, 1);
-    }
-`
-
 export const horizontalShake = (d: number) => {
     d = Math.abs(d)
     return keyframes`
@@ -85,26 +61,3 @@ export const verticalShake = (d: number) => {
         }
     `
 }
-
-export const wobble = keyframes`
-    0%,
-    100% {
-        transform: translateX(0%);
-        transform-origin: 50% 50%;
-    }
-    15% {
-        transform: translateX(-30px) rotate(-6deg);
-    }
-    30% {
-        transform: translateX(15px) rotate(6deg);
-    }
-    45% {
-        transform: translateX(-15px) rotate(-3.6deg);
-    }
-    60% {
-        transform: translateX(9px) rotate(2.4deg);
-    }
-    75% {
-        transform: translateX(-6px) rotate(-1.2deg);
-    }
-`
