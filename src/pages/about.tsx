263a891b61efc9112cb85c0813a0dd5258e39c24
Julien Graziano <julien@graziano.fr>
2022-07-22 23:30:56 +0200

    refactor: update about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,9 @@
-import styled from "@emotion/styled"
-
 import * as React from "react"
 
 import Layout from "../components/layout"
 
 // markup
-const IndexPage = () => <Layout pageTitle="About Me">
+const About = () => <Layout pageTitle="About Me">
     <h1>About Me</h1>
     <p>
         Hi there! I'm a software engineer with a passion for building things.
@@ -13,4 +11,4 @@ const IndexPage = () => <Layout pageTitle="About Me">
 </Layout>
 
 
-export default IndexPage
+export default About
