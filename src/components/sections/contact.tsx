a5e7211595b967d619fcf3a374955ff73cf0ac77
Julien Graziano <julien@graziano.fr>
2022-07-24 16:45:15 +0200

    refactor: update contact section

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -1,15 +1,21 @@
-import * as React from "react"
+/** @jsx jsx */
+
+import {
+    jsx,
+} from "@emotion/react"
 
 import {
     StyledSection,
 } from "./styled-section"
 
-export const Contact = () => <StyledSection id="contact">
-    <h1>Contact</h1>
-    <p>
-        I am not currently looking for new opportunities.
-    </p>
-    <p>
-        However if you have any questions, feel free to <a href="mailto:julien@graziano.fr">email me</a>.
-    </p>
-</StyledSection>
+export const Contact = () => {
+    return <StyledSection id="contact" css={{ textAlign: "center" }}>
+        <h1>Contact</h1>
+        <p>
+            I am not currently looking for new opportunities.
+        </p>
+        <p>
+            However if you have any questions, feel free to <a href="mailto:julien@graziano.fr">email me</a>.
+        </p>
+    </StyledSection>
+}
