e7bf784dbe60f63bd84684291c8542a723bc7112
Julien Graziano <julien@graziano.fr>
2022-08-13 09:05:41 +0200

    refactor: remove @fontsource/rokkitt

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,6 +1,3 @@
-import "@fontsource/rokkitt/300.css"
-import "@fontsource/rokkitt/400.css"
-
 import wrapWithProvider from "./src/wrap-with-provider"
 
 export const wrapRootElement = wrapWithProvider
