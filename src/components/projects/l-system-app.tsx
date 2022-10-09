d60a9daa07852dbe19312593b53b4b32b16e33d9
Julien Graziano <julien@graziano.fr>
2022-10-09 22:31:42 +0200

    refactor: update l-system project page

--- src/components/projects/l-system-app.tsx
+++ src/components/projects/l-system-app.tsx
@@ -19,6 +19,7 @@ import {
     defineLSystemRenderingRules,
     type ILSystemWord,
     type ILSystemProductionRulesMap,
+    type ILSystemRenderAction,
     type ILSystemRenderingRulesMap,
 } from "@nealrame/react-l-system"
 
@@ -27,6 +28,7 @@ import {
 } from "@/style"
 
 import InputRange from "@/components/ui/input-range"
+import Tabs from "@/components/ui/tabs"
 
 interface LSystemProps {
     axiom: ILSystemWord,
@@ -37,15 +39,137 @@ interface LSystemProps {
     stepsMax?: number,
 }
 
-const LSystemWrapper = styled.div`
+const LSystemWord = ({
+    word,
+}: { word: ILSystemWord }) => {
+    return <span>{ `${word.join(' ')}` }</span>
+}
+
+const LSystemAxiom = ({
+    axiom,
+}: { axiom: ILSystemWord }) => {
+    return <>
+        <label>Axiom</label>
+        <code>
+            <LSystemWord word={ axiom } />
+        </code>
+    </>
+}
+
+const LSystemProductionRules = ({
+    productionRules,
+}: { productionRules: ILSystemProductionRulesMap }) => {
+    return <>
+        <label>Production rules</label>
+        <code>
+            <ul css={{
+                listStyle: "none",
+                margin: 0,
+                padding: 0,
+                "& > li": {
+                    display: "flex",
+                    gap: "1ch",
+                }
+            }}>
+                { Object.entries(productionRules).map(([ key, value ]) => {
+                    return <li key={key}>
+                        <span>{ key }</span>
+                        <span>{ ' -> ' }</span>
+                        <LSystemWord word={ value } />
+                    </li>
+                }) }
+            </ul>
+        </code>
+    </>
+}
+
+const LSystemRenderAction = ({
+    action,
+}: { action: ILSystemRenderAction }) => {
+    if (action[0] === "forward") {
+        return <span>{ `draw forward of ${action[1]} unit(s)` }</span>
+    }
+    if (action[0] === "move") {
+        return <span>{ `move forward of ${action[1]} unit(s)` }</span>
+    }
+    if (action[0] === "turn") {
+        const angle = Math.abs(180*action[1]/Math.PI).toFixed(2)
+        const direction = action[1] < 0 ? "left" : "right"
+        return <span>{ `turn ${direction} ${angle}°` }</span>
+    }
+    return <span>{ action[0] }</span>
+}
+
+const LSystemRenderingRules = ({
+    renderingRules,
+}: { renderingRules: ILSystemRenderingRulesMap }) => {
+    return <>
+        <label>Rendering rules</label>
+        <code>
+            <ul css={{
+                listStyle: "none",
+                margin: 0,
+                padding: 0,
+                "& > li": {
+                    display: "flex",
+                    gap: "1ch",
+                }
+            }}>
+                { Object.entries(renderingRules).map(([ key, action ]) => {
+                    return <li key={key}>
+                        <span>{ key }</span>
+                        <span>{ ' -> ' }</span>
+                        <LSystemRenderAction { ...{ action }} />
+                    </li>
+                }) }
+            </ul>
+        </code>
+    </>
+
+}
+
+const LSystemDescriptionWrapper = styled.div`
     display: flex;
 
-    gap: 1rem;
+    flex-direction: column;
+
+    font-size: 0.75rem;
+
+    & > label {
+        border-bottom: 1px solid ${props => (props.theme as ITheme).colors.border};
+
+        font-weight: bold;
+
+        padding: 0.25rem;
+
+        text-transform: uppercase;
+    }
+
+    & > code {
+        padding: 0.25rem;
+    }
+`
+
+const LSystemDescription = ({
+    axiom,
+    productionRules,
+    renderingRules,
+}: LSystemProps) => {
+    return <LSystemDescriptionWrapper>
+        <LSystemAxiom { ...{ axiom }}/>
+        <LSystemProductionRules { ...{ productionRules }}/>
+        <LSystemRenderingRules { ...{ renderingRules }}/>
+    </LSystemDescriptionWrapper>
+}
+
+const LSystemViewWrapper = styled.div`
+    display: flex;
+
+    gap: .5rem;
+
     flex-direction: column;
 
     margin: 0 auto;
-
-    width: 50%;
 
     & > label {
         font-size: .75rem;
@@ -53,12 +177,12 @@ const LSystemWrapper = styled.div`
     }
 `
 
-const LSystem = ({
-        axiom,
-        productionRules,
-        renderingRules,
-        ...config
-    }: LSystemProps) => {
+const LSystemView = ({
+    axiom,
+    productionRules,
+    renderingRules,
+    ...config
+}: LSystemProps) => {
     const {
         stepsInit,
         stepsMin,
@@ -66,12 +190,9 @@ const LSystem = ({
     } = { stepsMin: 0, stepsMax: 6, stepsInit: 4, ...config }
     const { lsystems: lsystemsTheme } = useTheme() as ITheme
     const [ steps, setSteps ] = React.useState(stepsInit)
-
-    return <LSystemWrapper>
+    return <LSystemViewWrapper>
         <LSystemSVGRenderer
-            axiom={ axiom }
-            productionRules={ productionRules }
-            renderingRules={ renderingRules }
+            { ...{ axiom, productionRules, renderingRules }}
             steps={ steps }
             backgroundColor={ lsystemsTheme.colors.background }
             strokeColor={ lsystemsTheme.colors.stroke }
@@ -86,7 +207,26 @@ const LSystem = ({
                 onChange={ e => setSteps(parseInt(e.target.value)) }
             />
         </label>
+    </LSystemViewWrapper>
+}
 
+const LSystemWrapper = styled.div`
+    margin: 0 auto;
+    width: 75%;
+`
+
+const LSystem = ({
+        axiom,
+        productionRules,
+        renderingRules,
+        ...config
+    }: LSystemProps) => {
+    const lsystem = { axiom, productionRules, renderingRules }
+    return <LSystemWrapper>
+        <Tabs>
+            <div title="view"><LSystemView { ...lsystem } { ...config}/></div>
+            <div title="description"><LSystemDescription { ...lsystem }/></div>
+        </Tabs>
     </LSystemWrapper>
 }
 
